eb30aaf8112989c11f9820b24051abb32ca8c8ce
Gokulnaath Govindaraj <gokulhallows003@gmail.com>
2025-07-16 17:41:13 -0400

    Added the logic for login page to verify the jwt generated token

--- src/main/java/com/teamseven07/todolist/Services/UserService.java
+++ src/main/java/com/teamseven07/todolist/Services/UserService.java
@@ -2,15 +2,22 @@ package com.teamseven07.todolist.Services;
 
 import com.teamseven07.todolist.Model.User;
 import com.teamseven07.todolist.Repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authManager;
+    private final JWTService jwtService;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, AuthenticationManager authManager, JWTService jwtService) {
         this.userRepository=userRepository;
+        this.authManager=authManager;
+        this.jwtService=jwtService;
     }
 
     private BCryptPasswordEncoder byCrypt= new BCryptPasswordEncoder(12);
@@ -18,5 +25,14 @@ public class UserService {
     public User register(User user){
         user.setPassword(byCrypt.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public String verify(User user) {
+        Authentication authentication=authManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUsername());
+        }
+        return "Failure";
     }
 }
