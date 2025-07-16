0c922a8652a41119fa0147d17417b1b0ed0afd31
Gokulnaath Govindaraj <gokulhallows003@gmail.com>
2025-07-16 17:40:10 -0400

    Created a new bean for authmanager and ecluded the registe and login urls from authentication with bearer token now we dont need to include the auth for these urls

--- src/main/java/com/teamseven07/todolist/Config/UserConfig.java
+++ src/main/java/com/teamseven07/todolist/Config/UserConfig.java
@@ -3,9 +3,11 @@ package com.teamseven07.todolist.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,9 +29,12 @@ public class UserConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(customizer-> customizer.disable())
-                .authorizeHttpRequests(request-> request.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(request-> request
+                        .requestMatchers("/register", "/login")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form->form.disable())
+                .httpBasic(httpBasic->httpBasic.disable())
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
@@ -40,5 +45,10 @@ public class UserConfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception{
+        return config.getAuthenticationManager();
     }
 }
